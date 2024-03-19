a34a8006c026331a8f472c68e38730114c14b62e
Ivan Sabido <ivan.sabido@rappi.com>
2024-03-18 22:56:52 -0600

    first commit

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,58 @@
+import  { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider } from '../../Context'
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import SignIn from '../SignIn'
+import NotFound from '../NotFound'
+import Navbar from '../../Components/Navbar'
+import '../../App.css'
+
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/my-orders',
+      element: <MyOrders />,
+    },
+    {
+      path: '/my-order',
+      element: <MyOrder />,
+    },
+    {
+      path: '/my-account',
+      element: <MyAccount />,
+    },
+    {
+      path: '/signin',
+      element: <SignIn />,
+    },
+    {
+      path: '/*',
+      element: <NotFound />,
+    }
+    
+  ])
+
+  return routes
+}
+
+const App = () => {
+
+  return (
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes />
+        <Navbar />
+      </BrowserRouter>
+    </ShoppingCartProvider>
+    
+  )
+}
+
+export default App
